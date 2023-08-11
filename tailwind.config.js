20f2f31787034a7b8db6c30f207f6f062102f795
Rupom <mohrajulrupom@gmail.com>
2023-08-11 21:31:29 +0600

    set all

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [ 'index.html',
+  './src/**/*.{html,js}',],
+  theme: {
+    colors:{
+      'darkblue_1' : 'rgb(0, 54, 117)',
+    },
+    extend: {
+      
+    },
+  },
+  plugins: [],
+}
+
